4b5f0e9bf53b4b6fa43fd23732cfe75143021e82
Thomas Hernandez <peardian@gmail.com>
2012-11-26 15:29:50 -0500

    starting Outbound Core

---
+++ includes/classes/AmazonPackageTracker.php
@@ -0,0 +1,23 @@
+<?php
+
+class AmazonPackageTracker extends AmazonOutboundCore{
+    private $xmldata;
+    
+    /**
+     * Fetches a plan from Amazon. This is how you get a Shipment ID.
+     * @param string $s name of store as seen in config file
+     * @param boolean $mock true to enable mock mode
+     * @param array $m list of mock files to use
+     */
+    public function __construct($s, $mock = false, $m = null) {
+        parent::__construct($s, $mock, $m);
+        include($this->config);
+        
+        $this->options['Action'] = 'GetPackageTrackingDetails';
+        
+        $this->throttleLimit = $throttleLimitInventory;
+        $this->throttleTime = $throttleTimeInventory;
+    }
+    
+}
+?>

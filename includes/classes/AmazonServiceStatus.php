c0942d0a90f9d7409945d1ab59d64dc467ccfa39
Thomas Hernandez <peardian@gmail.com>
2012-12-20 11:10:31 -0500

    service status test, reports core tests complete

--- includes/classes/AmazonServiceStatus.php
+++ includes/classes/AmazonServiceStatus.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * A simple object that fetches the status of the a specific service from Amazon.
+ * Fetches the status of the a specific service from Amazon.
  * 
+ * This Amazon Core object retrieves the status of a selected Amazon service.
  * Please note that it has a 5 minute throttle time.
  */
 class AmazonServiceStatus extends AmazonCore{
     private $lastTimestamp;
     private $status;
+    private $messageId;
+    private $messageList;
+    private $ready = false;
     
     /**
      * A simple object that fetches the status of the Inventory API from Amazon
@@ -34,39 +38,19 @@ class AmazonServiceStatus extends AmazonCore{
     }
     
     /**
-     * Fetches the status of the service from Amazon
+     * Set the service to fetch the status of.
+     * @param string $s service name
+     * @return boolean true if success, false if failure
      */
-    public function fetchServiceStatus(){
-        $this->options['Timestamp'] = $this->genTime();
-        
-        $url = $this->urlbase.$this->urlbranch;
-        $this->options['Signature'] = $this->_signParameters($this->options, $this->secretKey);
-        $query = $this->_getParametersAsString($this->options);
-        
-        $path = $this->options['Action'].'Result';
-        if ($this->mockMode){
-            $xml = $this->fetchMockFile();
-        } else {
-            $this->throttle();
-            $this->log("Making request to Amazon");
-            $response = fetchURL($url,array('Post'=>$query));
-            $this->logRequest();
-            
-            if (!$this->checkResponse($response)){
-                return false;
-            }
-            
-            $xml = simplexml_load_string($response['body']);
-        }
-        
-        $this->lastTimestamp = (string)$xml->$path->Timestamp;
-        $this->status = (string)$xml->$path->Status;
-    }
-    
     public function setService($s){
         if (file_exists($this->config)){
             include($this->config);
         } else {
+            return false;
+        }
+        
+        if (is_null($s)){
+            $this->log("Service cannot be null",'Warning');
             return false;
         }
         
@@ -79,38 +63,103 @@ class AmazonServiceStatus extends AmazonCore{
             case 'Inbound':
                 $this->urlbranch = 'FulfillmentInboundShipment/'.$versionInbound;
                 $this->options['Version'] = $versionInbound;
+                $this->ready = true;
                 return true;
             case 'Inventory':
                 $this->urlbranch = 'FulfillmentInventory/'.$versionInventory;
                 $this->options['Version'] = $versionInventory;
+                $this->ready = true;
                 return true;
             case 'Orders':
                 $this->urlbranch = 'Orders/'.$versionOrders;
                 $this->options['Version'] = $versionOrders;
+                $this->ready = true;
                 return true;
             case 'Outbound':
                 $this->urlbranch = 'FulfillmentOutboundShipment/'.$versionOutbound;
                 $this->options['Version'] = $versionOutbound;
+                $this->ready = true;
                 return true;
             case 'Products':
                 $this->urlbranch = 'Products/'.$versionProducts;
                 $this->options['Version'] = $versionProducts;
+                $this->ready = true;
                 return true;
             case 'Sellers':
                 $this->urlbranch = 'Sellers/'.$versionSellers;
                 $this->options['Version'] = $versionSellers;
+                $this->ready = true;
                 return true;
             default:
+                $this->log("$s is not a valid service",'Warning');
                 return false;
         }
         
     }
     
     /**
-     * returns the fetched service status, and fetches it if it has not done so already
+     * Fetches the status of the service from Amazon
+     */
+    public function fetchServiceStatus(){
+        if (!$this->ready){
+            $this->log("Service must be set in order to retrieve status",'Warning');
+            return false;
+        }
+        $this->options['Timestamp'] = $this->genTime();
+        
+        $url = $this->urlbase.$this->urlbranch;
+        $this->options['Signature'] = $this->_signParameters($this->options, $this->secretKey);
+        $query = $this->_getParametersAsString($this->options);
+        
+        $path = $this->options['Action'].'Result';
+        if ($this->mockMode){
+            $xml = $this->fetchMockFile()->$path;
+        } else {
+            $this->throttle();
+            $this->log("Making request to Amazon");
+            $response = fetchURL($url,array('Post'=>$query));
+            $this->logRequest();
+            
+            if (!$this->checkResponse($response)){
+                return false;
+            }
+            
+            $xml = simplexml_load_string($response['body'])->$path;
+        }
+        
+        $this->parseXML($xml);
+    }
+    
+    protected function parseXML($xml){
+        if (!$xml){
+            return false;
+        }
+        $this->lastTimestamp = (string)$xml->Timestamp;
+        $this->status = (string)$xml->Status;
+        
+        if ($this->status == 'GREEN_I'){
+            $this->messageId = (string)$xml->MessageId;
+            $i = 0;
+            foreach ($xml->Messages->children() as $x){
+                $this->messageList[$i] = (string)$x->Text;
+                $i++;
+            }
+        }
+    }
+    
+    /**
+     * Returns whether or not the object is ready to retrieve the status.
+     * @return boolean
+     */
+    public function isReady(){
+        return $this->ready;
+    }
+    
+    /**
+     * returns the fetched service status if set
      * @return string|boolean false if status not yet retrieved
      */
-    public function getServiceStatus(){
+    public function getStatus(){
         if (isset($this->status)){
             return $this->status;
         } else {
@@ -119,12 +168,36 @@ class AmazonServiceStatus extends AmazonCore{
     }
     
     /**
-     * returns the timestamp of the last response, and fetches it if it has not done so already
-     * @return string|boolean false if status not yet retrieved
+     * returns the timestamp of the last response if set
+     * @return string|boolean false if timestamp not yet retrieved
      */
     public function getTimestamp(){
-        if (isset($this->status)){
+        if (isset($this->lastTimestamp)){
             return $this->lastTimestamp;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * returns the message ID of the last response if set
+     * @return string|boolean false if message ID not retrieved
+     */
+    public function getMessageId(){
+        if (isset($this->messageId)){
+            return $this->messageId;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * returns the message list of the last response if set
+     * @return string|boolean false if message list not retrieved
+     */
+    public function getMessageList(){
+        if (isset($this->messageList)){
+            return $this->messageList;
         } else {
             return false;
         }

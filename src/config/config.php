af9e1fa308560aea3eaee55fbfe98b0a9d3aaa3d
Tom Coonen <tom@creacoon.nl>
2015-04-29 14:05:46 +0200

    Config added

---
+++ src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'store' => [
+		'drhittich' => [
+			'merchantId' => '',
+			'marketplaceId' => '',
+			'keyId' => '',
+			'secretKey' => ''
+		]
+	],
+
+	//Service URL Base
+	'AMAZON_SERVICE_URL' => 'https://mws-eu.amazonservices.com/',
+
+	'muteLog' => false
+];

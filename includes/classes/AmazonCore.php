e146a103de02c66429e6c2d79ef552683e8ff6a5
lukes <lukestorer.reviews@gmail.com>
2017-04-21 17:00:27 +0100

    [bug] - added conditional check for index existence before trying to reference it.

--- includes/classes/AmazonCore.php
+++ includes/classes/AmazonCore.php
@@ -607,7 +607,7 @@ abstract class AmazonCore{
         $this->log("Making request to Amazon: ".$this->options['Action']);
         $response = $this->fetchURL($url,$param);
         
-        while ($response['code'] == '503' && $this->throttleStop==false){
+        while (isset($response['code']) && $response['code'] == '503' && $this->throttleStop==false){
             $this->sleep();
             $response = $this->fetchURL($url,$param);
         }

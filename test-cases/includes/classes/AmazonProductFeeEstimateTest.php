f2dcfa7fe6b3691215515d01585da6fac431ba64
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2018-01-20 04:27:15 +0800

    Update AmazonProductFeeEstimate.php (#147)
    * Update AmazonProductFeeEstimate.php
    
    Edited the option key for the listing prices so the amazon can get the data. Refer to the scratchpad product api labels.
    
    * Update AmazonProductFeeEstimateTest.php

--- test-cases/includes/classes/AmazonProductFeeEstimateTest.php
+++ test-cases/includes/classes/AmazonProductFeeEstimateTest.php
@@ -62,10 +62,10 @@ class AmazonProductFeeEstimateTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('ASIN', $o[$pre.'1.IdType']);
         $this->assertArrayHasKey($pre.'1.IdValue', $o);
         $this->assertEquals('B00123ASIN', $o[$pre.'1.IdValue']);
-        $this->assertArrayHasKey($pre.'1.ListingPrice.CurrencyCode', $o);
-        $this->assertEquals('USD', $o[$pre.'1.ListingPrice.CurrencyCode']);
-        $this->assertArrayHasKey($pre.'1.ListingPrice.Value', $o);
-        $this->assertEquals('123', $o[$pre.'1.ListingPrice.Value']);
+        $this->assertEquals('USD', $o[$pre.'1.PriceToEstimateFees.ListingPrice.CurrencyCode']);
+        $this->assertArrayHasKey($pre.'1.PriceToEstimateFees.ListingPrice.CurrencyCode', $o);
+        $this->assertArrayHasKey($pre.'1.PriceToEstimateFees.ListingPrice.Amount', $o);
+        $this->assertEquals('123', $o[$pre.'1.PriceToEstimateFees.ListingPrice.Amount']);
         $this->assertArrayHasKey($pre.'1.Identifier', $o);
         $this->assertEquals('TEST123', $o[$pre.'1.Identifier']);
         $this->assertArrayHasKey($pre.'1.IsAmazonFulfilled', $o);
